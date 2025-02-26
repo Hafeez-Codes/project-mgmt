78a2d07e19e3c3fba6c209e1596c47e4ea26fd0e
Popoola Abdulhafeez <hafeezolayinka2005@gmail.com>
2025-02-26 11:07:57 +0100

    Add routing and new pages for project details and home view

--- client/src/queries/projectQueries.js
+++ client/src/queries/projectQueries.js
@@ -1,52 +1,72 @@
 import { gql } from '@apollo/client';
 
 export const GET_PROJECTS = gql`
-    query GetProjects {
-        projects {
-            id
-            name
-            status
-        }
-    }
+	query GetProjects {
+		projects {
+			id
+			name
+			status
+		}
+	}
 `;
 
-export const GET_PROJECT_BY_ID = gql`
-    query GetProjectById($id: ID!) {
-        project(id: $id) {
-            id
-            name
-            description
-            status
-        }
-    }
+export const GET_PROJECT = gql`
+	query GetProject($id: ID!) {
+		project(id: $id) {
+			id
+			name
+			description
+			status
+			client {
+				id
+				name
+				email
+				phone
+			}
+		}
+	}
 `;
 
 export const CREATE_PROJECT = gql`
-    mutation CreateProject($name: String!, $description: String!, $status: String!) {
-        createProject(name: $name, description: $description, status: $status) {
-            id
-            name
-            description
-            status
-        }
-    }
+	mutation CreateProject(
+		$name: String!
+		$description: String!
+		$status: String!
+	) {
+		createProject(name: $name, description: $description, status: $status) {
+			id
+			name
+			description
+			status
+		}
+	}
 `;
 
 export const UPDATE_PROJECT = gql`
-    mutation UpdateProject($id: ID!, $name: String, $description: String, $status: String) {
-        updateProject(id: $id, name: $name, description: $description, status: $status) {
-            id
-            name
-            description
-            status
-        }
-    }
+	mutation UpdateProject(
+		$id: ID!
+		$name: String
+		$description: String
+		$status: String
+	) {
+		updateProject(
+			id: $id
+			name: $name
+			description: $description
+			status: $status
+		) {
+			id
+			name
+			description
+			status
+		}
+	}
 `;
 
 export const DELETE_PROJECT = gql`
-    mutation DeleteProject($id: ID!) {
-        deleteProject(id: $id) {
-            id
-        }
-    }
+	mutation DeleteProject($id: ID!) {
+		deleteProject(id: $id) {
+			id
+		}
+	}
 `;
