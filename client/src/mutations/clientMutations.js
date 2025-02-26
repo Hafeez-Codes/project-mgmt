50c720f29fcb83ee9b7a5f7e1074ee2f65c02d6e
Popoola Abdulhafeez <hafeezolayinka2005@gmail.com>
2025-02-26 09:49:02 +0100

    Add client management features with modal for adding clients and update cache on mutations

--- client/src/mutations/clientMutations.js
+++ client/src/mutations/clientMutations.js
@@ -1,6 +1,17 @@
 import { gql } from '@apollo/client';
 
-export const DELETE_CLIENT = gql`
+const ADD_CLIENT = gql`
+	mutation AddClient($name: String!, $email: String!, $phone: String!) {
+		addClient(name: $name, email: $email, phone: $phone) {
+			id
+			name
+			email
+			phone
+		}
+	}
+`;
+
+const DELETE_CLIENT = gql`
 	mutation DeleteClient($id: ID!) {
 		deleteClient(id: $id) {
 			id
@@ -10,3 +21,5 @@ export const DELETE_CLIENT = gql`
 		}
 	}
 `;
+
+export { DELETE_CLIENT, ADD_CLIENT };
